047e034198139d753e684ac04b2c48b3a74e4809
Guch1120 <e1x22089@oit.ac.jp>
2025-11-13 20:10:44 +0900

    setup_detic.bashの動作確認完了！ detic構築についてreadme.mdの更新が後で必要

--- setup_detic.bash
+++ setup_detic.bash
@@ -3,20 +3,24 @@ set -e
 
 echo "Detic 環境構築スクリプトを開始します。"
 
-# 仮想環境の作成を推奨 (オプション)
-# echo "Python仮想環境の作成を推奨します。"
-# echo "python3 -m venv venv"
-# echo "source venv/bin/activate"
-# read -p "仮想環境を有効化したら、Enterキーを押してください..."
-
 # 1. 作業ディレクトリの作成と移動
-mkdir -p ./detic
+if [! -d "./detic" ]; then
+    echo "作業ディレクトリ 'detic' を作成しています..."
+    mkdir detic
+else
+    echo "'detic'は既に存在"
+fi
+
 cd ./detic
 echo "作業ディレクトリ: $(pwd)"
 
 # 2. Detectron2 のクローンとインストール
-echo "Detectron2 をクローンしています..."
-git clone git@github.com:facebookresearch/detectron2.git
+if [! -d "./detectron2" ]; then
+    echo "Detectron2 をクローンしています..."
+    git clone git@github.com:facebookresearch/detectron2.git
+else
+    echo "Detectron2 ディレクトリは既に存在します。スキップします。"
+fi
 cd detectron2
 
 echo "PyTorch と Torchvision をインストールしています..."
@@ -39,8 +43,12 @@ cd ..
 echo "Detectron2 のインストールが完了しました。"
 
 # 3. Detic のクローンとインストール
-echo "Detic をクローンしています (サブモジュール含む)..."
-git clone git@github.com:facebookresearch/Detic.git --recurse-submodules
+if [! -d "./Detic" ]; then
+    echo "Detic をクローンしています (サブモジュール含む)..."
+    git clone git@github.com:facebookresearch/Detic.git --recurse-submodules 
+else
+    echo "Detic ディレクトリは既に存在します。スキップします。"
+fi
 cd Detic
 
 echo "Detic の要求ライブラリをインストールしています..."
@@ -49,16 +57,25 @@ pip install -r requirements.txt
 echo "Detic のインストールが完了しました。"
 echo "環境構築が正常に完了しました。"
 
+if [! -d "./models" ]; then
+    echo "モデル保存用ディレクトリ 'models' を作成しています..."
+    mkdir models
+else
+    echo "モデル保存用ディレクトリ 'models' は既に存在します。"
+fi
+wget https://dl.fbaipublicfiles.com/detic/Detic_LCOCOI21k_CLIP_SwinB_896b32_4x_ft4x_max-size.pth -P models/
+
+
+
 # 4. サンプル実行方法 (参考)
 # モデルやサンプル画像は別途ダウンロード/配置が必要です。
 : '
 # モデルのダウンロード (例)
-wget https://dl.fbaipublicfiles.com/detic/Detic_LCOCOI21k_CLIP_SwinB_896b32_4x_ft4x_max-size.pth -P models/
 
-# サンプル画像の配置 (例)
+# サンプル画像の配置 (例) なんでもいいから好きなやつをmodels/の中に入れて
 # (./models/sample.JPG に画像を配置する)
 
-# デモの実行
+# デモの実行はこれ． --input のあとのパスがあっているか確認してね
 python3 demo.py \
     --config-file configs/Detic_LCOCOI21k_CLIP_SwinB_896b32_4x_ft4x_max-size.yaml \
     --input ./models/sample.JPG \
@@ -66,3 +83,5 @@ python3 demo.py \
     --vocabulary lvis \
     --opts MODEL.WEIGHTS models/Detic_LCOCOI21k_CLIP_SwinB_896b32_4x_ft4x_max-size.pth
 '
+
+#実行にはsudo chown -R $USER:$USER ./deticが必要
