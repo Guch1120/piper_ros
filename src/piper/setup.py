c2b5b07c0aee07179213a6670a0a62d257ef049b
Guch1120 <e1x22089@oit.ac.jp>
2025-11-30 21:49:56 +0900

    コードからMoveit動作成功． flexbe追加． detic_rosのtf更新問題・moveitでtfから動作に取り組む

--- src/piper/setup.py
+++ src/piper/setup.py
@@ -30,7 +30,8 @@ setup(
             'piper_single_ctrl = piper.piper_single_ctrl_node:main',
             'piper_read_slave_joint = piper.piper_read_slave_joint:main',
             'piper_single_ctrl_moveit = piper.piper_single_ctrl_moveit_node:main',
-            'piper_moveit_bridge = piper.moveit_bridge:main'
+            'piper_moveit_bridge = piper.moveit_bridge:main',
+            'moveit_client = piper.moveit_client_node:main'
         ],
     },
 )
