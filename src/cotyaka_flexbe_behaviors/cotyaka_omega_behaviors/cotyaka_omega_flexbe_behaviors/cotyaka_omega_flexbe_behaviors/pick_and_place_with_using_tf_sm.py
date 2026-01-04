e467d3d50f940473c6876d5dc36f16319e1ad645
Guch1120 <e1x22089@oit.ac.jp>
2026-01-04 20:59:31 +0900

    tfで掴みに行くのはできた． realsenseのdepthがガバガバ． 掴む前に再度tf出して精度上げる必要アリ．

--- src/cotyaka_flexbe_behaviors/cotyaka_omega_behaviors/cotyaka_omega_flexbe_behaviors/cotyaka_omega_flexbe_behaviors/pick_and_place_with_using_tf_sm.py
+++ src/cotyaka_flexbe_behaviors/cotyaka_omega_behaviors/cotyaka_omega_flexbe_behaviors/cotyaka_omega_flexbe_behaviors/pick_and_place_with_using_tf_sm.py
@@ -79,17 +79,17 @@ class pick_and_place_with_using_TFSM(Behavior):
     def create(self):
         # x:830 y:332, x:483 y:347
         _state_machine = OperatableStateMachine(outcomes=['finished', 'failed'])
-        _state_machine.userdata.target_frame = ""
+        _state_machine.userdata.target_frame = "target"
 
         # Additional creation code can be added inside the following tags
         # [MANUAL_CREATE]
 
         # [/MANUAL_CREATE]
         with _state_machine:
-            # x:114 y:178
+            # x:125 y:75
             OperatableStateMachine.add('move_serch',
                                        MoveItJointClientParamState(group_name='arm', joint_names=['joint1','joint2','joint3','joint4','joint5','joint6'], target_joints=[0.0,0.34,-0.65,0.0,0.96,0.0], tolerance=0.01, action_topic='move_action'),
-                                       transitions={'reached': 'finished', 'failed': 'failed'},
+                                       transitions={'reached': 'sam3_test', 'failed': 'failed'},
                                        autonomy={'reached': Autonomy.Off, 'failed': Autonomy.Off})
 
             # x:377 y:70
