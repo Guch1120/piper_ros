56be478cb13d655ef94132b7a238a97518bc0124
Guch1120 <e1x22089@oit.ac.jp>
2026-01-05 05:07:30 +0900

    アームぷるぷる問題が解決できるかも？

--- src/piper/setup.py
+++ src/piper/setup.py
@@ -31,12 +31,13 @@ setup(
             'piper_read_slave_joint = piper.piper_read_slave_joint:main',
             'piper_single_ctrl_moveit = piper.piper_single_ctrl_moveit_node:main',
             'piper_moveit_bridge = piper.moveit_bridge:main',
+            'piper_moveit_bridge_smooth = piper.moveit_bridge_smoth:main',
             'moveit_client = piper.moveit_client_node:main ',
             'moveit_client_tf = piper.moveit_client_tf_node:main',
             'moveit_client_tf_interactive = piper.moveit_client_tf_interactive:main',
             'pick_and_place_trajectory = piper.pick_and_place_trajectry:main',
             'gripper_open_test = piper.gripper_open_test_node:main',
-            'gripper_close_test = piper.gripper_close_test_node:main'
+            'gripper_close_test = piper.gripper_close_test_node:main',
         ],
     },
 )
